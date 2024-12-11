1eb4ec7f96b322781df39cf807c94902e0404c79
aliceinthyme <alice.neuhaus@protonmail.com>
2024-12-11 10:36:19 +0100

    makes a script executable and moves it to ~/bin

---
+++ mkscript.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $# -ne 1 ]
+then
+    echo "Invalid Parameters"
+    exit 1
+fi
+if [ ! -d ~/bin ]
+then
+    mkdir ~/bin
+fi
+if [ -e $1 ]
+then
+    chmod u+x $1
+    if [ -e ~/bin/$1 ]
+    then
+        mv $1 ~/bin/${1}_new
+        echo "file with identical name exists in ~/bin, this file was moved with additional _new suffix to avoid overwriting, it will not be executable until manual intervention"
+    else
+        mv $1 ~/bin
+    fi
+fi
