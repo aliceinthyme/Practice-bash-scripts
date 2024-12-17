be8bc7f16f0265a315469b84a12164864743cb0a
aliceinthyme <alice.neuhaus@protonmail.com>
2024-12-17 08:40:46 +0100

    --

--- mkscript.sh
+++ mkscript.sh
@@ -10,12 +10,13 @@ then
 fi
 if [ -f $1 ]
 then
-    chmod u+x $1
     if [ -e ~/bin/$1 ]
     then
         mv $1 ~/bin/${1}_new
+        chmod u+x ~/bin/${1}_new
         echo "file with identical name exists in ~/bin, this file was moved with additional _new suffix to avoid overwriting, it will not be executable until manual intervention"
     else
+        chmod u+x $1
         mv $1 ~/bin
     fi
 fi
